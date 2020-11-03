7d5530142a372abcb4fc55a713f99492ec936bc0
DESKTOP-2KR6A3P\user <1281747107@qq.com>
2020-11-03 10:51:50 +0800

    Initial commit

---
+++ splashActivity/src/main/java/com/base/BaseFragment.java
@@ -0,0 +1,18 @@
+package com.base;
+
+import com.base.listeners.IBaseActivity;
+import com.base.util.BaseCommTool;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public abstract class BaseFragment  extends Fragment implements IBaseActivity  {
+	public int scW; //屏幕宽度
+	public int scH; //屏幕高度
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		scW = BaseCommTool.getScreenWidth(getActivity());
+		scH = BaseCommTool.getScreenHeight(getActivity());
+	}
+}
